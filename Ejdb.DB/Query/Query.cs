0a8032c23dc2b6990819b38cc4cd24383e150753
Oliver Klemencic <oliver.klemencic@gmail.com>
2013-12-27 18:45:56 +0100

    Added further query helpers

--- Ejdb.DB/Query/Query.cs
+++ Ejdb.DB/Query/Query.cs
@@ -21,43 +21,86 @@ namespace Ejdb.DB
             return new Query(fieldName, value);
         }
 
+        public static IQuery EqualsIgnoreCase(string fieldName, string value)
+        {
+            return _BinaryQuery("$icase", fieldName, value);
+        }
+
+        public static IQuery BeginsWith(string fieldName, string value)
+        {
+            return _BinaryQuery("$begin", fieldName, value);
+        }
+
+        public static IQuery EndsWith(string fieldName, string value)
+        {
+            return _BinaryQuery("end", fieldName, value);
+        }
+
         public static IQuery GT(string fieldName, object value)
         {
-            return BinaryQuery("$gt", fieldName, value);
+            return _BinaryQuery("$gt", fieldName, value);
         }
 
         public static IQuery GTE(string fieldName, object value)
         {
-            return BinaryQuery("$gte", fieldName, value);
+            return _BinaryQuery("$gte", fieldName, value);
         }
 
         public static IQuery LT(string fieldName, object value)
         {
-            return BinaryQuery("$lt", fieldName, value);
+            return _BinaryQuery("$lt", fieldName, value);
         }
 
         public static IQuery LTE(string fieldName, object value)
         {
-            return BinaryQuery("$lte", fieldName, value);
+            return _BinaryQuery("$lte", fieldName, value);
         }
 
         public static IQuery In<T>(string fieldName, params T[] comparisonValues)
         {
-            return BinaryQuery("$in", fieldName, comparisonValues);
+            return _BinaryQuery("$in", fieldName, comparisonValues);
+        }
+
+        public static IQuery NotIn<T>(string fieldName, params T[] comparisonValues)
+        {
+            return _BinaryQuery("$nin", fieldName, comparisonValues);
         }
 
         public static IQuery Not(string fieldName, object comparisonValue)
         {
-            return BinaryQuery("$not", fieldName, comparisonValue);
+            return _BinaryQuery("$not", fieldName, comparisonValue);
         }
+
+        /* public static IQuery Not(string fieldName, IQuery query)
+        {
+            var childValue = query.GetQueryDocument();
+            return new Query("$not", childValue);
+        } */
 
         public static IQuery Between<T>(string fieldName, T comparisonValue1, T comparisonValue2)
         {
             var comparisonValues = new T[] { comparisonValue1, comparisonValue2 };
-            return BinaryQuery("$bt", fieldName, comparisonValues);
+            return _BinaryQuery("$bt", fieldName, comparisonValues);
         }
 
-        public static IQuery BinaryQuery(string queryOperation, string fieldName, object comparisonValue)
+        public static IQuery And(params IQuery[] queries)
+        {
+            return _CombinedQuery("$and", queries);
+        }
+
+        public static IQuery Or(params IQuery[] queries)
+        {
+            return _CombinedQuery("$or", queries);
+        }
+
+        private static IQuery _CombinedQuery(string combinator, IQuery[] queries)
+        {
+            var documents = queries.Select(x => x.GetQueryDocument()).ToArray();
+            var childValue = new BsonArray(documents);
+            return new Query(combinator, childValue);
+        }
+
+        private static IQuery _BinaryQuery(string queryOperation, string fieldName, object comparisonValue)
         {
             var query1 = new BsonDocument();
             query1[queryOperation] = comparisonValue;
